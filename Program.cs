c0a2689b210d321bd0ab85a43d54040e19048243
Maksym Isaiev <46455130+IsaevMaksym@users.noreply.github.com>
2023-06-19 18:24:35 +0300

    UC#10 Implement regex

--- Program.cs
+++ Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Text.RegularExpressions;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Please enter your string:");
+        string userInput = Console.ReadLine();
+
+        Console.WriteLine("Please enter the maximum allowed string length:");
+        var isNumberValid = Int32.TryParse(Console.ReadLine(), out int maxLength);
+
+        if (isNumberValid && IsValid(userInput, maxLength))
+        {
+            Console.WriteLine("Your string is valid.");
+        }
+        else
+        {
+            Console.WriteLine("Your inputs are invalid.");
+        }
+
+        Console.ReadLine();
+    }
+
+    public static bool IsValid(string input, int maxLength)
+    {
+        // Define the regular expression that validates the string requirements.
+        string pattern = @"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!""#$%&'()*+,-./:;<=>?@[\]^_`{|}~])[A-Za-z\d!""#$%&'()*+,-./:;<=>?@[\]^_`{|}~]{1," + maxLength + "}$";
+
+        Regex regex = new Regex(pattern);
+        return regex.IsMatch(input);
+    }
+}
